8cb289fee6fab2df41370a2d4f1b19d8b7c3355b
Lucas <lucasrocini@hotmail.com>
2021-08-29 01:03:18 -0300

    First Look of Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is a sample test', 
+      'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_1280.jpg'
+    ),
+    new Recipe(
+      'A test Recipe 2',
+      'This is a sample test 2', 
+      'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_1280.jpg'
+    )
+  ];
 
   constructor() { }
 
